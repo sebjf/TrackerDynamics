c60f69d5619ca01998f39f7833843d3e06221fa1
Simon Julier <s.julier@ucl.ac.uk>
2021-10-08 18:12:09 +0100

    Plot quaternions. Use thicker lines in xyz plot.

--- Matlab/plotPositions.m
+++ Matlab/plotPositions.m
@@ -11,8 +11,8 @@ for i = ids'
     C = T(T.id==i,:); % controller table
     x = C.time;
    
-    plot(x,C.position); labels=[labels, "x","y","z"];
-    plot(x(2:end),diff(C.position)); labels=[labels,"dx","dy","dz"];
+    plot(x,C.position, 'LineWidth', 2); labels=[labels, "x","y","z"];
+    plot(x(2:end),diff(C.position), 'LineWidth', 2); labels=[labels,"dx","dy","dz"];
     
     state_change = diff(C.word) ~= 0;
     mark_times = [mark_times; C.time(state_change)]; 
@@ -26,4 +26,23 @@ end
 
 legend(labels);
 
+
+figure;
+labels = [];
+for i = ids'
+    C = T(T.id==i,:); % controller table
+    x = C.time;
+   
+    plot(x,abs(C.rotation), 'LineWidth', 2); labels=[labels, "w", "x","y","z"];
+    %plot(x(2:end),diff(C.position), 'LineWidth', 2); labels=[labels,"dx","dy","dz"];
+    
+    state_change = diff(C.word) ~= 0;
+    mark_times = [mark_times; C.time(state_change)]; 
 end
+
+for t = mark_times'
+   h = line([t t],ylim);
+   h.Color = [0 0 0];
+   h.LineStyle = ':';
+end
+
